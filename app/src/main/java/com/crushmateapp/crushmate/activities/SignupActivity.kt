9c6e0cbd5f400cc234ede079ea75b7fa419c6896
Amitoz singh <52659877+amitozs999@users.noreply.github.com>
2019-11-10 19:09:55 +0530

    commit

---
+++ app/src/main/java/com/crushmateapp/crushmate/activities/SignupActivity.kt
@@ -0,0 +1,13 @@
+package com.crushmateapp.crushmate.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.crushmateapp.crushmate.R
+
+class SignupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
---
+++ app/src/main/java/com/crushmateapp/crushmate/activities/StartActivity.kt
@@ -0,0 +1,14 @@
+package com.crushmateapp.crushmate.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.crushmateapp.crushmate.R
+
+class StartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
+    }
+}
